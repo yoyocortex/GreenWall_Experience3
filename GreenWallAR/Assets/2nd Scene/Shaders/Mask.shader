9092e1f4539286e10cdef576ec77d823d69ac786
yoyocortex <22plepelic22@gmail.com>
2023-05-16 01:29:49 +0200

    Initial commit

---
+++ GreenWallAR/Assets/2nd Scene/Shaders/Mask.shader
@@ -0,0 +1,15 @@
+Shader "Custom/Mask"
+{
+    SubShader
+    {
+        // draw after all opaque objects (queue = 2001):
+        Tags
+        {
+            "Queue"="Transparent+1"
+        }
+        Pass
+        {
+            Blend Zero One // keep the image behind it
+        }
+    }
+}
---
+++ GreenWallAR/Assets/Scripts/PlaceTrackedImages.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARTrackedImageManager))]
+public class PlaceTrackedImages : MonoBehaviour
+{
+    private ARTrackedImageManager _trackedImagesManager;
+    public GameObject[] ArPrefabs;
+
+    private readonly Dictionary<string, GameObject> _instantiatedPrefabs = new Dictionary<string, GameObject>();
+
+     void Awake()
+    {
+        _trackedImagesManager = GetComponent<ARTrackedImageManager>();
+    }
+    private void OnEnable()
+    {
+        _trackedImagesManager.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+    private void OnDisable()
+    {
+        _trackedImagesManager.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+    private void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (var trackedImage in eventArgs.added)
+        {
+            var imageName = trackedImage.referenceImage.name;
+            foreach (var curPrefab in ArPrefabs)
+            {
+                if (string.Compare(curPrefab.name, imageName, StringComparison.OrdinalIgnoreCase) == 0
+                    && !_instantiatedPrefabs.ContainsKey(imageName))
+                {
+                    var newPrefab = Instantiate(curPrefab, trackedImage.transform);
+                    _instantiatedPrefabs[imageName] = newPrefab;
+                }
+            }
+            _instantiatedPrefabs[trackedImage.referenceImage.name].SetActive(trackedImage.trackingState == TrackingState.Tracking);
+        }
+
+
+        foreach (var trackedImage in eventArgs.removed) {
+            // Destroy its prefab
+            Destroy(_instantiatedPrefabs[trackedImage.referenceImage.name]);
+            // Also remove the instance from our array
+            _instantiatedPrefabs.Remove(trackedImage.referenceImage.name);
+            // Or, simply set the prefab instance to inactive
+            //_instantiatedPrefabs[trackedImage.referenceImage.name].SetActive(false);
+        }
+    }
+}
